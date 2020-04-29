adc90c86e39e072cb0ff3cd757da6cfed4052881
Felipe Matheus <felipemateus174@gmail.com>
2020-04-29 14:50:10 -0300

    teste 2

--- organizer.py
+++ organizer.py
@@ -2,12 +2,12 @@ import os
 import glob
 import shutil
 from datetime import datetime
-for folders in glob.glob ('*.txt'):
+for folders in glob.glob ('*.pdf'):
         created = os.stat(folders).st_mtime
         getdate = (datetime.fromtimestamp(created))
         mon = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
-        path =  str (getdate.year) + "/" + str (getdate.day ) +"_"+ str (mon[getdate.month])
+        path =  str (getdate.year) + "/" + str (getdate.day ) +"_"+ str (mon[getdate.month]) + "/" + str (folders[path.glob])
         print ( path )
-        os.makedirs( path )
+        os.makedirs( path, exist_ok=True )
         shutil.move (folders, path)
         
